1ed4df2dd3d36f308e7fc2ee432240aa06620c61
Sushmoy Roy <sushmoyr@gmail.com>
2023-06-11 13:59:46 +0600

    Added DB migrations and settings

---
+++ AspBlog/Data/BlogDbContext.cs
@@ -0,0 +1,15 @@
+using AspBlog.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspBlog.Data;
+
+public class BlogDbContext: DbContext
+{
+    public BlogDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<BlogPost> BlogPosts { get; set; }
+    
+    public DbSet<Tag> Tags { get; set; }
+}
--- AspBlog/Program.cs
+++ AspBlog/Program.cs
@@ -1,7 +1,13 @@
+using AspBlog.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<BlogDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("BlogDbConnection")));
 
 var app = builder.Build();
 
