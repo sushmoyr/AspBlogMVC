8aaf876c455d871fca1d2604d5496c07cdb242a4
Sushmoy Roy <sushmoyr@gmail.com>
2023-06-13 14:37:23 +0600

    added dropdown in navbar with admin menu options

--- AspBlog/Controllers/HomeController.cs
+++ AspBlog/Controllers/HomeController.cs
@@ -1,6 +1,6 @@
 ﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
 using AspBlog.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AspBlog.Controllers;
 
--- AspBlog/Data/BlogDbContext.cs
+++ AspBlog/Data/BlogDbContext.cs
@@ -3,13 +3,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace AspBlog.Data;
 
-public class BlogDbContext: DbContext
+public class BlogDbContext : DbContext
 {
     public BlogDbContext(DbContextOptions options) : base(options)
     {
     }
 
     public DbSet<BlogPost> BlogPosts { get; set; }
-    
+
     public DbSet<Tag> Tags { get; set; }
 }
